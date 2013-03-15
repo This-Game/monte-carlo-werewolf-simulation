62ea7496899239d24d225cebf0d42b0713ed212f
Patrick Ewing <patrick.henry.ewing@gmail.com>
2013-03-14 21:09:26 -0700

    Renaming

--- simulate.rb
+++ simulate.rb
@@ -31,22 +31,24 @@ def simulate runs, *player_classes
   pp player_classes.to_s
   hist.each_pair do |player_type, wins|
     share = (wins.to_f / runs) * 100
-
     pp "#{player_type}: #{wins} wins, #{share.round(2)}%"
-    # delta = v - share
-    # deltap = (delta.to_f / runs) * 100
-    # puts "%+0.2f%% %s" % [deltap, k]
   end
 
   puts "avg rounds: #{ rounds.to_f / runs.to_f }"
   puts
 end
 
+simulate 5000, Player, Player, Player,  Werewolf
+simulate 5000, Player, Player, Player, Player, Player, Player, Player, Player, Player, Player,Player, Player, Player, Player, Player,Player, Player, Player, Player, Player, Player, Werewolf
 simulate 5000, Player, Player, Player, Player, Player, Werewolf
-simulate 5000, Player, Player, Player, Player, Player, Werewolf, Werewolf
-simulate 5000, Player, Player, Player, Player, Player, Player, Player, Player, Player, Player, Werewolf, Werewolf
+simulate 5000, Player, Player, Player, Player, Player, Werewolf
+simulate 5000, Player, Player, Player, Player, Player, Werewolf
+simulate 5000, Player, Player, Player, Player, Player, Werewolf
+simulate 5000, Player, Player, Player, Player, Player, Werewolf
+simulate 5000, Player, Player, Player, Player, Player, Werewolf
 
 
 
-# simulate 10000, Player, Player, Player, Player, Player, Werewolf, Werewolf, Werewolf
+# simulate 5000, Player, Player, Player, Player, Player, Werewolf, Werewolf
+# simulate 5000, Player, Player, Player, Player, Player, Player, Player, Player, Player, Player, Werewolf, Werewolf
 
